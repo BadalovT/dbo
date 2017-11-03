93676061d2c4c341c1fc42b8a682b6608afd43d6
BadalovT <bedelov.tebriz@gmail.com>
2017-11-03 21:40:43 +0400

    Add files via upload

---
+++ dbo.Table_4.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].Flight
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Status] INT NOT NULL, 
+    [No] NVARCHAR(50) NOT NULL, 
+    [CompanyId] INT NOT NULL, 
+    [CityId] INT NOT NULL, 
+    [AirportId] INT NOT NULL, 
+    [Type] INT NOT NULL, 
+    [GateId] INT NOT NULL, 
+    [RunwayId] INT NOT NULL, 
+    [FlightDate] DATETIME NOT NULL, 
+    [Late] INT NOT NULL, 
+    CONSTRAINT [FK_Flight_Companies] FOREIGN KEY ([CompanyId]) REFERENCES Companies([id])
+)
